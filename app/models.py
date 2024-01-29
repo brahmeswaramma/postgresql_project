c252acef3470e225854bfa6b2a478d043b87dddf
brahmeswaramma <brahmeswarammaathinti@gamil.com>
2024-01-29 19:14:09 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class School(models.Model):
+    School_name=models.CharField(max_length=100)
+    School_principal=models.CharField(max_length=100)
+    School_location=models.CharField(max_length=100)
+
+
